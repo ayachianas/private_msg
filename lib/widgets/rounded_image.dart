9e788a5f76955034b5abbd576960e11c506d5c7a
ayachianas <ayachianas97@yahoo.fr>
2022-02-24 18:10:29 +0100

    Added RoundedImageNetwork and RoundedImageFile widgets.

---
+++ lib/widgets/rounded_image.dart
@@ -0,0 +1,61 @@
+// Pckages
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+
+
+class RoundedImageNetwork extends StatelessWidget {
+  final String imagePath;
+  final double size;
+
+  RoundedImageNetwork({
+    required Key key,
+    required this.imagePath,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            imagePath,
+          ),
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(size),),
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class RoundedImageFile extends StatelessWidget {
+  final PlatformFile image;
+  final double size;
+
+  RoundedImageFile({
+    required Key key,
+    required this.image,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: FileImage(File(image.path!)),
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(size),),
+        color: Colors.black,
+      ),
+    );
+  }
+}
