99e91ccb1c80627e21e8cd1b9dd701175268e996
ayachianas <ayachianas97@yahoo.fr>
2022-03-03 17:49:02 +0100

    Added the option to switch between Chats and Users pages.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
 // Packages
 import 'package:flutter/material.dart';
 
+//Pages
+import 'package:private_msg/pages/chats_page.dart';
+import 'package:private_msg/pages/users_page.dart';
+
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,12 +14,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentPage = 0;
+  final List<Widget> _pages = [
+    ChatsPage(),
+    UsersPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        onTap: (_index) {
+          setState(() {
+            _currentPage = _index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: "Chats",
+            icon: Icon(Icons.chat_bubble_sharp),
+          ),
+          BottomNavigationBarItem(
+            label: "Users",
+            icon: Icon(Icons.supervised_user_circle_sharp),
+          ),
+        ],
+      ),
+    );
   }
 }
