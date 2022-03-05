d3ca85fd24188e0c5a86d7e3334aef6925573a76
ayachianas <ayachianas97@yahoo.fr>
2022-03-05 16:15:50 +0100

    Added streamMessagesForChat, addMessageToChat, updateChatData and deleteChat methods.

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,6 +1,9 @@
 // Packages
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// Models
+import 'package:private_msg/models/chat_message.dart';
+
 const String USER_COLLECTION = "users";
 const String CHAT_COLLECTION = "chats";
 const String MESSAGES_COLLECTION = "messages";
@@ -45,11 +48,48 @@ class DataBaseService {
         .get();
   }
 
+  Stream<QuerySnapshot> streamMessagesForChat(String _chatID) {
+    return _db
+        .collection(CHAT_COLLECTION)
+        .doc(_chatID)
+        .collection(MESSAGES_COLLECTION)
+        .orderBy("sent_time", descending: false)
+        .snapshots();
+  }
+
+  Future<void> addMessageToChat(String _chatID, ChatMessage _message) async {
+    try {
+      await _db
+          .collection(CHAT_COLLECTION)
+          .doc(_chatID)
+          .collection(MESSAGES_COLLECTION)
+          .add(_message.toJson());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> updateChatData(String _chatID, Map<String, dynamic> _data) async {
+    try {
+      await _db.collection(CHAT_COLLECTION).doc(_chatID).update(_data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   Future<void> updateUserLastSeenTime(String _uid) async {
     try {
       await _db.collection(USER_COLLECTION).doc(_uid).update({
         "last_active": DateTime.now().toUtc(),
       });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> deleteChat(String _chatID) async {
+    try {
+      await _db.collection(CHAT_COLLECTION).doc(_chatID).delete();
     } catch (e) {
       print(e);
     }
