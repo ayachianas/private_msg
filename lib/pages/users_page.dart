6e44b14037eb25a11e86cc0ae75ba0c330ffd92c
ayachianas <ayachianas97@yahoo.fr>
2022-03-10 18:05:48 +0100

    Updated users page.

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+// Providers
+import 'package:private_msg/providers/authentication_provider.dart';
+import 'package:private_msg/providers/users_page_provider.dart';
+
+// Widgets
+import 'package:private_msg/widgets/top_bar.dart';
+import 'package:private_msg/widgets/custom_input_fields.dart';
+import 'package:private_msg/widgets/custom_list_view_tiles.dart';
+import 'package:private_msg/widgets/rounded_button.dart';
+
+// Models
+import 'package:private_msg/models/chat_user.dart';
 
 class UsersPage extends StatefulWidget {
   const UsersPage({Key? key}) : super(key: key);
@@ -12,14 +24,131 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+  late UsersPageProvider _pageProvider;
+
+  final TextEditingController _searchFieldTextEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return _buildUI();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UsersPageProvider>(
+          create: (_) => UsersPageProvider(_auth),
+        ),
+      ],
+      child: _buildUI(),
+    );
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      backgroundColor: Colors.green,
+    return Builder(
+      builder: (BuildContext _context) {
+        _pageProvider = _context.watch<UsersPageProvider>();
+        return Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.03,
+            vertical: _deviceHeight * 0.02,
+          ),
+          height: _deviceHeight * 0.98,
+          width: _deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TopBar(
+                'Users',
+                primaryAction: IconButton(
+                  icon: Icon(
+                    Icons.logout,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () => _auth.logout(),
+                ),
+              ),
+              CustomTextField(
+                onEditingComplete: (_value) {
+                  _pageProvider.getUsers(name: _value);
+                  FocusScope.of(context).unfocus();
+                },
+                hintText: "Search...",
+                obscureText: false,
+                controller: _searchFieldTextEditingController,
+                icon: Icons.search,
+              ),
+              _usersList(),
+              _createChatButton(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _usersList() {
+    List<ChatUser>? _users = _pageProvider.users;
+    return Expanded(
+      child: () {
+        if (_users != null) {
+          if (_users.length != 0) {
+            return ListView.builder(
+              itemCount: _users.length,
+              itemBuilder: (BuildContext _context, int _index) {
+                return CustomListViewTile(
+                  height: _deviceHeight * 0.1,
+                  title: _users[_index].name,
+                  subtitle: "Last Active: ${_users[_index].lastDayActive()}",
+                  imagePath: _users[_index].imgURL,
+                  isActive: _users[_index].wasRecentlyActive(),
+                  isSelected:
+                      _pageProvider.selectedUsers.contains(_users[_index]),
+                  onTap: () =>
+                      _pageProvider.updateSelectedUsers(_users[_index]),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: Text(
+                "No users found.",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            );
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.grey,
+            ),
+          );
+        }
+      }(),
+    );
+  }
+
+  Widget _createChatButton() {
+    return Visibility(
+      visible: _pageProvider.selectedUsers.isNotEmpty,
+      child: RoundedButton(
+        name: _pageProvider.selectedUsers.length == 1
+            ? "Chat with ${_pageProvider.selectedUsers.first.name}"
+            : "Create group chat",
+        height: _deviceHeight * 0.08,
+        width: _deviceWidth * 0.8,
+        onPressed: () {
+          _pageProvider.createChat();
+        },
+      ),
     );
   }
 }
